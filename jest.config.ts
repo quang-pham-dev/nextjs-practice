0bb673b8abba30b5cbb8aefbe19707e9cafe7d2b
quangpn <quangpn101290@gmail.com>
2022-03-13 10:01:52 +0700

    setup testing with jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,23 @@ import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
 const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.js',
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$': `<rootDir>/test/__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/test/components/$1',
+
+    '^@/pages/(.*)$': '<rootDir>/test/pages/$1',
+  },
   transform: {
     '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
   },
---
+++ test/pages/index.test.tsx
@@ -0,0 +1,6 @@
+describe('Home page', () => {
+  it('matches snapshot', () => {
+    const test = 'this is the test string'
+    expect(test).toEqual('this is the test string')
+  })
+})
